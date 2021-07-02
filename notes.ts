ed1bdd5902db24d7da57058eb20fb70db289a168
LuanSSilva <52720974+luanpixel@users.noreply.github.com>
2021-07-02 14:05:49 -0300

    Initial commit

---
+++ notes.ts
@@ -0,0 +1,18 @@
+/**
+ * GET  => Buscar uma informação
+ * POST => Inserir(Criar) uma informação
+ * PUT  => Alterar uma informação
+ * DELETE => Remover um dado
+ * PATCH  => Alterar uma informação especifica
+ */
+
+/**
+ * Tipos de parâmetros:
+ * Routes Params => https://localhost:3000/produtos/252510
+ * Query Params => https://localhost:3000/produtos?name=teclado&description=tecladobom&
+ *
+ * Body Params => {
+ *  "name": "teclado",
+ *  "description": "teclado bom"
+ * }
+ */
